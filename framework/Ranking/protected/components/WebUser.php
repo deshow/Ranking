8de916e79bd0b0375e9a97ccc960be90126282c1
Park <k_park@hoo-no-MacBook-Air.local>
2013-09-01 14:06:16 +0900

    six

--- framework/Ranking/protected/components/WebUser.php
+++ framework/Ranking/protected/components/WebUser.php
@@ -2,26 +2,13 @@
 // this file must be stored in:
 // protected/components/WebUser.php
 class WebUser extends CWebUser {
-	
 	protected $role = 0;
-	protected $group = '';
-	
 	public function setRole($value){
 		Yii::app()->user->setState('role', $value);
 	}
-	
 	public function getRole(){
 		$role = Yii::app()->user->getState('role');
 		return (null!==$role)?$role:$this->role;
-	}
-	
-	public function setGroup($value){
-		Yii::app()->user->setState('group', $value);
-	}
-	
-	public function getGroup(){
-		$grp = Yii::app()->user->getState('group');
-		return (null!==$grp)?$grp:$this->group;
 	}
 }
 ?>
--- framework/Ranking/protected/controllers/RankingController.php
+++ framework/Ranking/protected/controllers/RankingController.php
@@ -26,23 +26,12 @@ class RankingController extends Controller
 	 */
 	public function accessRules()
 	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
+		$denyOrAllow = Yii::app()->user->getRole();
+		if($denyOrAllow > 1){
+			return array();
+		}
+		return array('deny',  // deny all users
+				'users'=>array('*'));
 	}
 
 	/**
@@ -123,7 +112,6 @@ class RankingController extends Controller
 	 */
 	public function actionIndex()
 	{
-		
 		$dataProvider=new CActiveDataProvider('Ranking');
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
